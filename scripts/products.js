33be857849a27ed9122570691b8640199b3c9a30
Destiny Moncrief <D.moncrief4@gmail.com>
2024-06-10 18:57:13 -0400

    made changes

--- scripts/products.js
+++ scripts/products.js
@@ -1,136 +1,118 @@
-"use strict";
+"use strict"
 
-function displayProductsByCategory(category) {
-    document.getElementById("products").innerHTML = "";
 
-    // Filter products by category
-    const filteredProducts = products.filter(product => product.category === category);
 
-    // Display filtered products
-    filteredProducts.forEach(product => {
-        const productDiv = document.createElement("div");
-        productDiv.classList.add("product");
-        productDiv.innerHTML = `
-            <h3>${product.name}</h3>
-            <p>ID: ${product.id}</p>
-            <p>Price: $${product.description}</p>
-            <a href="details.html?productId=${product.id}">See Details</a>
-        `;
-        document.getElementById("products").appendChild(productDiv);
+const searchDropDown = document.getElementById("searchOption");
+const categoryDropDown = document.getElementById("categoryDropdown")
+const categorySelectEl = document.getElementById("category"); 
+ let allProdDiv = document.getElementById("products");
+var baseUrl = "http://localhost:8081/api/"
 
-        // Add product selection dropdown for each product
-        const selectProduct = document.createElement("select");
-        selectProduct.className = "form form-control";
-        selectProduct.addEventListener("change", () =>
-            productSelection(selectProduct.value)
-        );
+window.onload = function() { //the logic that goes inside of the window.onload only loads once
+    console.log("app is running");
+   
+    populateCategoryDropdown();
 
-        for (let product of products) {
-            const productOption = document.createElement("option");
-            productOption.textContent = product.name;
-            productOption.value = product.name;
-            selectProduct.appendChild(productOption);
+    searchDropDown.addEventListener("change", (event) => {
+
+        if(searchDropDown.value == "selectOne"){
+
+            selectOneDisplay(event);
+        } else if(searchDropDown.value == "products"){
+            viewAllDisplay();
+        } else if(searchDropDown.value == "categories"){
+            categoryDropdownDisplay();
         }
+      
+     
 
-        // Append selectProduct dropdown to productDiv
-        productDiv.appendChild(selectProduct);
-    });
+    })
 }
 
-// Event listener for category selection change
-document.getElementById("category").addEventListener("change", function() {
-    const selectedCategory = this.value;
-    displayProductsByCategory(selectedCategory);
-});
 
-function productSelection(selectedProductName) {
-    console.log(selectedProductName);
-    const selectedProduct = products.find(product => product.name === selectedProductName);
-    if (selectedProduct) {
-        displayProductDetails(selectedProduct.id);
-    } else {
-        console.error('Selected product not found.');
-    }
+// here we are making the function that will be called when users select the 'Select one..' option
+//from the search Drop down
+function selectOneDisplay(event){
+    event.preventDefault();
+
+    categoryDropDown.style.display = "none";
+    allProdDiv.style.display = "none";
+
 }
 
-// Define a function to display product details
-function displayProductDetails(productId) {
-    const apiUrl = `http://localhost:8081/api/products/${productId}`;
 
-    // Fetch product details from the API
-    fetch(apiUrl)
-        .then(response => response.json())
-        .then(product => {
-            // Display the product details
-            displayProduct(product);
-        })
-        .catch(error => {
-            console.error('Error fetching product details:', error);
-        });
-}
+// this logic will display all products when a user selects the 'View All' option from the search drop down
+function viewAllDisplay(){
 
-function displayProduct(product) {
-    
     clearProductDetails();
 
-    
-    const productDetailsDiv = document.createElement('div');
-    productDetailsDiv.className = 'product-details';
 
-    // Populate product details
-    productDetailsDiv.innerHTML = `
-        <h3>${product.name}</h3>
-        <p>ID: ${product.id}</p>
-        <p>Price: $${product.price}</p>
-        <p>Description: ${product.description}</p>
+    categoryDropDown.style.display = "none";
+    allProdDiv.style.display = "block";
+
+    //make a fetch call to http://localhost:8081/api/products
+    //why? because in the server.js file this endpoint was defined 
+    //and the response that we get when we send a request to this api is
+    //and array of products
+
+let newUrl = baseUrl + searchDropDown.value;
+console.log(newUrl)
+fetch(newUrl)
+.then(response => response.json())
+.then(allProducts => {
+
+
+    allProducts.forEach( product => {
+    const productDiv = document.createElement("div");
+    productDiv.classList.add("product");
+    productDiv.innerHTML = `
+        <h3>${product.productName}</h3>
+        <p>ID: ${product.productId}</p>
+        <p>Price: $${product.unitPrice}</p>
+        <a href="details.html?productId=${product.productId}">See Details</a>
     `;
+  
+    allProdDiv.appendChild(productDiv);
+})
 
-   
-    document.getElementById('productDetailsContainer').appendChild(productDetailsDiv);
+});
+
 }
 
+function categoryDropdownDisplay(){
+    categoryDropDown.style.display = "block";
+    allProdDiv.style.display = "none";
+}
+
+
+function populateCategoryDropdown(){
+let newUrl= baseUrl + "categories"
+    //send a get request to http://localhost:8081/api/categories
+    //the response will be the array of categories
+    //forEach category in the array
+    //we will create a new option elemnt and add to the category drop down select element
+
+
+
+    fetch (newUrl) 
+    .then(response => response.json())
+    .then( allCategories => 
+    
+
+    allCategories.forEach(category => {
+        let option = document.createElement("option")
+        option.innerHTML = category.name;
+        option.value = category.categoryId;
+
+       categorySelectEl.appendChild(option);
+    
+
+    }
+)
+);
+
+}
 function clearProductDetails() {
-    const container = document.getElementById('productDetailsContainer');
+    const container = document.getElementById('products');
     container.innerHTML = '';
 }
-
-// make table for product
-let newApi = data.properties.products;
-fetch(newApi)
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
-    const table = document.createElement("table");
-    table.className = "table border mt-5";
-    const tableHeader = document.createElement("thead");
-    tableHeader.className = "table-dark";
-    const tableBody = document.createElement("tbody");
-    const headerRow = document.createElement("tr");
-    ["Name", "Category Id", "Description",].forEach((text) => {
-      const header = document.createElement("th");
-      header.textContent = text;
-      headerRow.appendChild(header);
-    });
-    tableHeader.appendChild(headerRow);
-    table.appendChild(tableHeader);
-    for (let values of data.properties.periods) {
-      const row = document.createElement("tr");
-      const cell = document.createElement("td");
-      cell.textContent = values.name;
-      const cell2 = document.createElement("td");
-      cell2.textContent = `Name: ${values.name} ${values.productName}`;
-      const cell3 = document.createElement("td");
-      cell3.textContent = `Category Id: ${values.Category} ${values.CategoryId}`;
-      const cell4 = document.createElement("td");
-      cell4.textContent = `Description: ${values.productDescription}`;
-
-      row.appendChild(cell);
-      row.appendChild(cell2);
-      row.appendChild(cell3);
-      row.appendChild(cell4);
-      tableBody.appendChild(row);
-    }
-    table.appendChild(tableBody);
-    tableDiv.appendChild(table);
-  });
-
